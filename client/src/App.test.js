4085746b1391f9da3eb4858674f59edaa814a628
Watson Chao <yinchuan2001@gmail.com>
2025-11-24 11:47:21 -0500

    fix: update tests to match calculator app

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders calculator', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const clearButton = screen.getByText(/clear/i);
+  expect(clearButton).toBeInTheDocument();
+});
+
+test('renders calculator display', () => {
+  render(<App />);
+  const displays = screen.getAllByText('0');
+  expect(displays.length).toBeGreaterThan(0);
 });
